2de8364c59cad78b733e7fffeef4b3da3f0bb3a0
Shaikh Nihaal <nihaalshaikh06@gmail.com>
2023-04-11 10:47:38 +0530

    .mjs import export examples

---
+++ module1.mjs
@@ -0,0 +1,3 @@
+import abc, {a, b, c} from './module2.mjs';
+console.log(abc);
+console.log(a);
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+const a = "Nihaal";
+const b = "Jack";
+const c = "John";
+
+export default b;
+export {a, b, c};
