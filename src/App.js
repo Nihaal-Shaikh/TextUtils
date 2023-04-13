ea6f4a46d1e168c6702809aeacefae7179fc8adb
Shaikh Nihaal <nihaalshaikh06@gmail.com>
2023-04-13 09:15:24 +0530

    Minor SEO improvements and Title changed according to toggle

--- src/App.js
+++ src/App.js
@@ -28,13 +28,23 @@ function App() {
     {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success")
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils  - Dark Mode";
+      /* Used to get attention of the user. Bad UI UX
+      setInterval(() => {
+        document.title = 'TextUtils is Amazing';
+      }, 2000);
+      setInterval(() => {
+        document.title = 'Install TextUtils Now!';
+      }, 1500);
+      */
     }
     else
     {
       setMode('light');
       document.body.style.backgroundColor = '#fff';
-      showAlert("Light mode has been enabled", "success")
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils  - Light Mode";
     }
   }
 
